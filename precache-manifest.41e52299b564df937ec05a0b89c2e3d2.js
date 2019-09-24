880c392cac0467eb9688eb14b9613f7fab9addf7
mohamed belkahla <mohamadbr22@gmail.com>
2019-09-24 17:15:36 +0100

    Updates

---
+++ precache-manifest.41e52299b564df937ec05a0b89c2e3d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c97a834fa96bc93f1b307436e469f5a",
+    "url": "/Todoist-Clone/index.html"
+  },
+  {
+    "revision": "581074a49f99fc745ba5",
+    "url": "/Todoist-Clone/static/js/2.83ade3a2.chunk.js"
+  },
+  {
+    "revision": "c98103ed0ad19091ab1c",
+    "url": "/Todoist-Clone/static/js/main.20b2b307.chunk.js"
+  },
+  {
+    "revision": "3736ccedfebf1e6beab2",
+    "url": "/Todoist-Clone/static/js/runtime~main.67692b6b.js"
+  }
+]);
